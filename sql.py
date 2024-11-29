95171eacedc1fbef51a98ec7bd099b5b4f4af8b8
SaiVeekshithKotha <ksv3306@gmail.com>
2024-11-29 18:47:58 +0530

    Realtor_Agency POV

--- sql.py
+++ sql.py
@@ -48,7 +48,76 @@ def View_Agency_Related_Properties(Agency_Id):
     try:
         cursor.execute(sql, (Agency_Id))
         connection.commit()
-        return cursor.fetchall()
+        # Print the result
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+    except PyMySQL.Error as e:
+        print(e)
+        return False
+    return True
+
+def View_Agency_Related_Clients(Agency_Id):
+    global cursor
+    sql = "SELECT c.Client_Id as ID, c.Name as Name , c.DateOfBirth as DOB , c.Contact_Info_Email as Email , c.Contact_Info_Phone as Phone FROM Client as c JOIN Agencies_Contacted as ac ON c.Client_ID = ac.Client_ID AND ac.Agency_Id = %s"
+
+    try:
+        cursor.execute(sql, (Agency_Id))
+        connection.commit()
+        # Print the result
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+            print("\n")
+            view_dep(row[0])
+    except PyMySQL.Error as e:
+        print(e)
+        return False
+    return True
+
+def View_Agency_Related_Owners(Agency_Id):
+    global cursor
+    sql = "SELECT o.Owner_id Owner_Id, o.Name Name, o.DOB DOB, o.Owner_Type Type FROM Owner as o JOIN Agencies_Employies as ae ON o.Owner_id = ae.Owner_ID AND ae.Agency_ID = %s"
+
+    try:
+        cursor.execute(sql, (Agency_Id))
+        connection.commit()
+        # Print the result
+        result = cursor.fetchall()
+        for row in result:
+            print(row)   
+    except PyMySQL.Error as e:
+        print(e)
+        return False
+    return True
+
+def View_Agency_Related_Realtors(Agency_Id):
+    global cursor
+    sql = "SELECT r.Employee_Id Employee_Id, r.Name Name, r.DateOfBirth DOB, r.Contact_Info_Email Email, r.Contact_Info_Phone Phone, r.S_Realtor_Id Supervisor_Id, r.Start_Date Start_Date, r.Experience Experience FROM Realtor as r WHERE r.Agency_Id = %s"
+
+    try:
+        cursor.execute(sql, (Agency_Id))
+        connection.commit()
+        # Print the result
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+    except PyMySQL.Error as e:
+        print(e)
+        return False
+    return True
+
+def View_Agency_Related_Realtors_With_Experience(Agency_Id , Experience):
+    global cursor
+    sql = "SELECT r.Employee_Id Employee_Id, r.Name Name, r.DateOfBirth DOB, r.Contact_Info_Email Email, r.Contact_Info_Phone Phone, r.S_Realtor_Id Supervisor_Id, r.Start_Date Start_Date, r.Experience Experience FROM Realtor as r WHERE r.Agency_Id = %s AND r.Experience > %s"
+
+    try:
+        cursor.execute(sql, (Agency_Id))
+        connection.commit()
+        # Print the result
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
     except PyMySQL.Error as e:
         print(e)
         return False
